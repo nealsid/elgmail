3eb90330c7f7ff75d57f3a8084db4d1098e6c4eb
Neal <nealsid@users.noreply.github.com>
2025-11-19 11:47:56 -0500

    First cut at batch requests
    Unfortunately I'm getting rate limited by Google when I run this
    function due to too many concurrent requests.

--- elgmail-batch-request.el
+++ elgmail-batch-request.el
@@ -10,9 +10,28 @@ GET /gmail/v1/users/me/labels
 --elgmailboundary--"))
   (url-retrieve-synchronously "https://www.googleapis.com/batch/gmail/v1"))
 
+(defun elgperf-fetch-threads-batch-request ()
+  (let ((threads (elg-get-threads-for-labels '("INBOX")))
+        (individual-requests (list)))
+    (seq-doseq (one-thread threads)
+      (push (format "--elgmailboundary
+Content-Type: application/http
+
+GET /gmail/v1/users/me/threads/%s
+
+" (gethash "id" one-thread)) individual-requests))
+    (let ((url-debug t)
+          (url-request-method "POST")
+          (url-request-extra-headers `(("Authorization" . ,(concat "Bearer " (oauth2-token-access-token elg--oauth-token)))
+                                       ("Content-Type" . "multipart/mixed; boundary=elgmailboundary")))
+          (url-request-data (concat
+                             (string-join individual-requests "")
+                             "--elgmailboundary--")))
+      (url-retrieve-synchronously "https://www.googleapis.com/batch/gmail/v1"))))
+
+  
 (defun elgperf-fetch-threads-individually ()
   (let ((threads (elg-get-threads-for-labels '("INBOX"))))
-    (message "%d threads" (length threads))
     (seq-doseq (one-thread threads)
       (elg-get-thread-by-id (gethash "id" one-thread)))))
 (benchmark-run 10
