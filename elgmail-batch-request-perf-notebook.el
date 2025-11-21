6752fe18a43f9ad9d759d13501d393b6cd6016f2
Neal <nealsid@users.noreply.github.com>
2025-11-21 16:39:27 -0500

    Disable debug logging option for url library
    - Fix elg-fetch-threads-by-id-batch to return hash table of threads
    - Some benchmark output

--- elgmail-batch-request-perf-notebook.el
+++ elgmail-batch-request-perf-notebook.el
@@ -28,8 +28,7 @@ GET /gmail/v1/users/me/threads/%s
     ;; bunch of request bodies.
     (seq-doseq (one-thread-id thread-ids)
       (push (format elgperf-embedded-http-request-format-string one-thread-id) individual-request-bodies))
-    (let ((url-debug t)
-          (url-request-method "POST")
+    (let ((url-request-method "POST")
           (url-request-extra-headers `(("Authorization" . ,(format "Bearer %s" (oauth2-token-access-token elg--oauth-token)))
                                        ("Content-Type" . "multipart/mixed; boundary=elgmailboundary")))
           (url-request-data (concat (string-join individual-request-bodies "") "--elgmailboundary--")))
@@ -44,7 +43,7 @@ GET /gmail/v1/users/me/threads/%s
             ;; (something like printable ascii characters, no spaces)
             (when (re-search-forward "^content-type: multipart/mixed; boundary=\\(.+\\)" nil t)
               (let ((boundary-marker (concat "--" (match-string 1)))
-                    (results-ht (make-hash-table)))
+                    (results-ht (make-hash-table :test 'equal)))
                 (puthash "200" (list) results-ht)
                 ;; Now that we've found the boundary string, iterate
                 ;; while it is found and parse the response
@@ -57,10 +56,9 @@ GET /gmail/v1/users/me/threads/%s
                     (backward-char)
                     (let ((json-begin (point)))
                       (re-search-forward "^}")
-                      (push (json-parse-string (buffer-substring json-begin (point))) (gethash "200" results-ht)))))))))))))
+                      (push (json-parse-string (buffer-substring json-begin (point))) (gethash "200" results-ht)))))
+                results-ht))))))))
             
-(elg-fetch-threads-by-id-batch '("199ea159ca464d4b"))
-(elg-fetch-threads-by-id-batch gmail-thread-ids)
 
 (defun elgperf-fetch-threads-batch-request ()
   (let ((threads (elg-get-threads-for-labels '("INBOX") 50))
@@ -89,5 +87,29 @@ GET /gmail/v1/users/me/threads/%s
       (url-debug t))
   (elgperf-fetch-threads-batch-request))
 
-(setq gmail-threads (elg-get-threads-for-labels '("INBOX") 5))
+(elg-fetch-threads-by-id-batch '("199ea159ca464d4b"))
+
+(setq gmail-threads (elg-get-threads-for-labels '("INBOX") 40))
 (setq gmail-thread-ids (mapcar (lambda (gmail-thread) (gethash "id" gmail-thread)) gmail-threads))
+(benchmark-elapse
+  (setq nsd-result (elg-fetch-threads-by-id-batch gmail-thread-ids)))
+;;0.520099
+(benchmark-run 10
+  (setq nsd-result (elg-fetch-threads-by-id-batch gmail-thread-ids)))
+(5.114594 9 0.7195630000000008)
+;;(4.714735 11 0.8250339999999987)
+
+(seq-doseq (tid gmail-thread-ids)
+  (elg-get-thread-by-id tid t))
+
+(benchmark-elapse
+  (mapcar (lambda (tid) (elg-get-thread-by-id tid t)) gmail-thread-ids))
+;;4.492557
+(benchmark-run 10
+  (mapcar (lambda (tid) (elg-get-thread-by-id tid t)) gmail-thread-ids))
+;;(41.833568 9 0.7834780000000023)
+
+(maphash (lambda (k v) (message "%s" k)) nsd-result)
+(length (gethash "200" nsd-result))
+(nth 39 (gethash "200" nsd-result))
+
